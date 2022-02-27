fad6618df18b88686a8968e94a0fa88946b46ed6
rtehok <tehok.raymond@gmail.com>
2022-02-27 18:29:11 +0100

    implement error channel in ourzio

--- src/main/scala/dev/rth/zioplayground/Main.scala
+++ src/main/scala/dev/rth/zioplayground/Main.scala
@@ -1,7 +1,7 @@
 package dev.rth
 package zioplayground
 
-import ourzio.*
+import dev.rth.zioplayground.ourzio.*
 
 @main def Main(args: String*): Unit =
   val trace = s"[${Console.BLUE}TRACE${Console.RESET}]"
@@ -10,11 +10,20 @@ import ourzio.*
     for
       _ <- console.putStrLn("─" * 100)
       _ <- console.putStrLn("What is your name?")
-//      name <- console.getStrLn
+      //      name <- console.getStrLn
       name <- ZIO.succeed("Ray")
       _ <- console.putStrLn(s"Hello $name")
+      //      _ <- ZIO.fail(throw RuntimeException("boom")) // don't throw, use effect instead
+      //      _ <- ZIO.fail(RuntimeException("boom")) // produces a ZIO[RuntimeException, Unit]
+      //      _ <- ZIO.effect(throw RuntimeException("boom")) // produces a ZIO[Throwable, Unit]
+//      _ <- ZIO
+//        .effect(throw RuntimeException("boom"))
+//        .catchAll(_ => console.putStrLn("error catch"))
+      _ <- ZIO
+        .effect(throw RuntimeException("boom"))
+        .mapError(_.getMessage)
       _ <- console.putStrLn("─" * 100)
     yield ()
 
-  Runtime.default.unsafeRunSync(program)
+  println(Runtime.default.unsafeRunSync(program))
 
--- src/main/scala/dev/rth/zioplayground/ourzio.scala
+++ src/main/scala/dev/rth/zioplayground/ourzio.scala
@@ -2,35 +2,69 @@ package dev.rth
 package zioplayground
 
 object ourzio:
-  final case class ZIO[A](thunk: () => A):
-    def flatMap[B](azb: A => ZIO[B]): ZIO[B] =
-      ZIO.succeed {
-        val a  = thunk()
-        val zb = azb(a)
-        val b = zb.thunk()
-        b
+  final case class ZIO[+E, +A](thunk: () => Either[E, A]):
+    def flatMap[E1 >: E, B](azb: A => ZIO[E1, B]): ZIO[E1, B] =
+      ZIO { () =>
+        val errorOrA = thunk()
+        val zErrorOrB = errorOrA match
+          case Right(a) => azb(a)
+          case Left(e) => ZIO.fail(e)
+
+        val errorOrB = zErrorOrB.thunk()
+        errorOrB
       }
-      
-    def map[B](ab: A => B): ZIO[B] =
-      ZIO.succeed {
-        val a  = thunk()
-        val b = ab(a)
-        b
-      }  
+
+    def map[B](ab: A => B): ZIO[E, B] =
+      ZIO { () =>
+        val errorOrA = thunk()
+        val errorOrB = errorOrA match
+          case Right(a) => Right(ab(a))
+          case Left(e) => Left(e) // left @ Left(e) => left.asInstanceOf[Either[E, B]]
+        errorOrB
+      }
+
+    def catchAll[E2, A1 >: A](h: E => ZIO[E2, A1]): ZIO[E2, A1] =
+      ZIO { () =>
+        val errorOrA = thunk()
+        val zErrorOrA1 = errorOrA match
+          case Right(a) => ZIO.succeed(a)
+          case Left(e) => h(e)
+
+        val errorOrA1 = zErrorOrA1.thunk()
+        errorOrA1
+      }
+
+    def mapError[E2](h: E => E2): ZIO[E2, A] =
+      ZIO { () =>
+        val errorOrA = thunk()
+        val error2OrA = errorOrA match
+          case Right(a) => Right(a)
+          case Left(e) => Left(h(e)) // left @ Left(e) => left.asInstanceOf[Either[E, B]]
+        error2OrA
+      }
   end ZIO
-  
+
   object ZIO:
-    def succeed[A](a: => A): ZIO[A] =
-      ZIO(() => a)
-      
+    def succeed[A](a: => A): ZIO[Nothing, A] =
+      ZIO(() => Right(a))
+
+    def fail[E](e: => E): ZIO[E, Nothing] =
+      ZIO(() => Left(e))
+
+    def effect[A](a: => A): ZIO[Throwable, A] =
+      ZIO { () =>
+        try Right(a)
+        catch ex => Left(ex)
+      }
+
   object console:
-    def putStrLn(line: String): ZIO[Unit] =
+    def putStrLn(line: String): ZIO[Nothing, Unit] =
       ZIO.succeed(println(line))
 
-    val getStrLn: ZIO[String] =
+    val getStrLn: ZIO[Nothing, String] =
       ZIO.succeed(scala.io.StdIn.readLine())
-      
+
   object Runtime:
     object default:
-      def unsafeRunSync[A](zio: => ZIO[A]): A =
+      def unsafeRunSync[E, A](zio: => ZIO[E, A]): Either[E, A] =
         zio.thunk()
